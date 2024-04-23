4081972b63d8549d24eed2409afe15322d022fc4
Tony Sharples <tonysharples22@me.com>
2024-04-23 13:15:37 +0100

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Tony Sharples on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
